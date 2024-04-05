5da106692b2653aba310b0dec4ecd72b2d8a062f
vratskyi <lesha0skorohod@gmail.com>
2024-04-05 14:32:49 +0300

    upd

---
+++ public/scripts/confetti.js
@@ -0,0 +1,40 @@
+document.addEventListener('astro:page-load', function () {
+  if (window.location.pathname === '/order-success/') {
+    // Создаем эффект конфети
+    let count = 600;
+    let defaults = {
+      origin: { y: 0.7 },
+    };
+
+    function fire(particleRatio, opts) {
+      confetti({
+        ...defaults,
+        ...opts,
+        particleCount: Math.floor(count * particleRatio),
+      });
+    }
+
+    fire(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+    fire(0.2, {
+      spread: 60,
+    });
+    fire(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 1.42,
+      scalar: 1.2,
+    });
+    fire(0.1, {
+      spread: 160,
+      startVelocity: 65,
+    });
+    }
+  });
