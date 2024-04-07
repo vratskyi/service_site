5ade2eb9c9c60b3a7643c9d9ffbd8d3e64ec721b
vratskyi <lesha0skorohod@gmail.com>
2024-04-07 13:34:54 +0300

    upd

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,64 +2,71 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import mdx from "@astrojs/mdx";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    compress({
-      CSS: false,
-      HTML: false,
-      Image: true,
-      JavaScript: true,
-      SVG: false,
-    }),
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: {
-        theme: "dracula",
+  site: 'https://vratsky.vercel.app',
+  integrations: [tailwind(), compress({
+    CSS: false,
+    HTML: false,
+    Image: true,
+    JavaScript: true,
+    SVG: false
+  }), mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "dracula"
+    },
+    // remarkPlugins: [remarkToc],
+    // rehypePlugins: [rehypeMinifyHtml],
+    // remarkRehype: { footnoteLabel: 'Footnotes' },
+    gfm: false
+  }), sitemap({
+    i18n: {
+      defaultLocale: 'ru',
+      locales: {
+        ru: 'ru-RU',
+        ua: 'uk-UA',
+        en: 'en-US',
       },
-      // remarkPlugins: [remarkToc],
-      // rehypePlugins: [rehypeMinifyHtml],
-      // remarkRehype: { footnoteLabel: 'Footnotes' },
-      gfm: false,
-    }),
-  ],
+    },
+  })],
   // i18n settings
   i18n: {
     defaultLocale: "ru",
     locales: ["ru", "ua", "en"],
     routing: {
       prefixDefaultLocale: true,
-      redirectToDefaultLocale: true,
+      redirectToDefaultLocale: true
     },
     fallback: {
       ua: "ru",
-      en: "ru",
-    },
+      en: "ru"
+    }
   },
   redirects: {
-    "/": "/ru",
+    "/": "/ru"
   },
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: "viewport",
+    defaultStrategy: "viewport"
   },
   experimental: {
     clientPrerender: true,
-    directRenderScript: false,
+    directRenderScript: false
   },
   output: "hybrid",
   adapter: vercel({
     functionPerRoute: true,
     imageService: true,
     isr: {
-      expiration: 60 * 60 * 24,
+      expiration: 60 * 60 * 24
     },
     webAnalytics: {
-      enabled: true,
-    },
-  }),
+      enabled: true
+    }
+  })
 });
